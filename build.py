f9004ebfad6a0d5ad345380161e798e97b0c22d9
Mark Jan van Kampen <mjvk@allseas.com>
2019-08-08 12:12:29 +0200

    Updates build.py

--- build.py
+++ build.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import os
-from bincrafters import build_template_installer
-from bincrafters import build_shared
-from conans import tools
+from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    arch = os.environ["ARCH"]
-    builder = build_template_installer.get_builder()
-    builder.add({"os" : build_shared.get_os(), "arch_build" : arch, "arch": arch}, {}, {}, {})
+    builder = ConanMultiPackager()
+    builder.add_common_builds(pure_c=True)
     builder.run()
